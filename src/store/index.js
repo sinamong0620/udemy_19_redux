d04e18dc65f3afbb55c432cf97776db4ba14fb29
sinamong0620 <sinaskim60@gmail.com>
2023-09-15 17:51:07 +0900

    toggle func

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,28 @@
 //리덕스 로직 저장할거임
 import { createStore } from "redux";
-
+const initialState = { counter: 0, showCounter: true };
 //리듀서 함수
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
-    return { counter: state.counter + 1 };
+    return { counter: state.counter + 1, showCounter: state.showCounter };
   }
   if (action.type === "increase") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
     };
   }
   return state;
